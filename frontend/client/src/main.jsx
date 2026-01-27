0f87aa8e768785821f96b6b4d7e6bdc5955bf6f7
Dhruv-2403 <dhruvsareen24@gmail.com>
2026-01-28 01:42:33 +0530

    Routes Section

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -1,25 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AppContextProvider } from './context/AppContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { Toaster } from 'react-hot-toast'
 
+// Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Clerk Publishable Key')
+  throw new Error("Missing Publishable Key")
 }
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <AppContextProvider>
         <App />
-        <Toaster position="top-right" />
       </AppContextProvider>
     </ClerkProvider>
-  </BrowserRouter>
+  </BrowserRouter>,
 )
